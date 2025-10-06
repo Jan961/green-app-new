e3c5287c2aa515fa5995b908f6bb788fadc04741
Jan <jan.wiecz961@gmail.com>
2025-10-06 11:02:52 +0100

    init

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,52 @@
+import 'vuetify/styles'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const lightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#1867C0',
+    'primary-darken-1': '#3700B3',
+    secondary: '#5CBBF6',
+    'secondary-darken-1': '#018786',
+    error: '#F44336',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#121212',
+    surface: '#121212',
+    primary: '#2196F3',
+    secondary: '#03DAC6',
+    error: '#CF6679',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'lightTheme',
+    themes: {
+      lightTheme,
+      darkTheme,
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+})
+
+export default vuetify
+
+
