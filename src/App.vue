75afba81a415142018b8d751007a014f19f6b4b7
Jan <jan.wiecz961@gmail.com>
2025-10-07 21:05:46 +0100

    hopefully fixed TS mem warning

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
 
 export default defineComponent({
   name: 'App',
-  components: { Footer },
+  components: { Footer, Header },
 })
 </script>
 
 <template>
   <v-app>
-    <v-app-bar flat>
-      <v-toolbar-title>Green App</v-toolbar-title>
-      <v-spacer />
-      <v-btn to="/about" variant="text">About</v-btn>
-      <v-btn to="/volunteer" variant="text">Volunteer</v-btn>
-      <v-btn to="/contact" variant="text">Contact</v-btn>
-      <v-btn to="/donate" color="primary" variant="flat">Donate</v-btn>
-    </v-app-bar>
+    <Header />
     <v-main>
       <RouterView />
     </v-main>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+        <v-app-bar flat>
+      <v-toolbar-title>Green App</v-toolbar-title>
+      <v-spacer />
+      <v-btn to="/about" variant="text">About</v-btn>
+      <v-btn to="/volunteer" variant="text">Volunteer</v-btn>
+      <v-btn to="/contact" variant="text">Contact</v-btn>
+      <v-btn to="/donate" color="primary" variant="flat">Donate</v-btn>
+    </v-app-bar>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+</style>
